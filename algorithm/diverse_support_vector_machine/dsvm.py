808a416166e1c94d1b372b8c58e997ef7e6d8b73
cperales <cperales@uloyola.es>
2017-11-07 13:26:03 +0100

    In DSVM, x and y changed to train_data and train_target

--- algorithm/diverse_support_vector_machine/dsvm.py
+++ algorithm/diverse_support_vector_machine/dsvm.py
@@ -14,8 +14,8 @@ class DiverseLinearSVM(Classifier):
     def __init__(self, **kwargs):
         self.c = kwargs['c'] if 'c' in kwargs else 1.0
         self.d = kwargs['d'] if 'd' in kwargs else 1.0
-        self.x = None
-        self.y = None
+        self.train_data = None
+        self.train_target = None
         self.w = None
         self.b = None
         self.prev_w = None
@@ -24,22 +24,22 @@ class DiverseLinearSVM(Classifier):
         """
         Fit the model according to the given training data.
         """
-        self.add_x_y(x=data, y=target)
+        self.add_train(train_data=data, train_target=target)
         # Data can be added as a pickle using read_data method
         if diverse is True:
-            alphas = fit_diverse(self.x, self.y, self.prev_w)
+            alphas = fit_diverse(self.train_data, self.train_target, self.prev_w)
         else:
             if soft is True:
-                alphas = fit_soft(self.x, self.y, self.c)
+                alphas = fit_soft(self.train_data, self.train_target, self.c)
             else:
-                alphas = fit(self.x, self.y)
+                alphas = fit(self.train_data, self.train_target)
 
         # get weights
-        w = np.sum(alphas * self.y[:, None] * self.x, axis=0)
+        w = np.sum(alphas * self.train_target[:, None] * self.train_data, axis=0)
         # # get b
         # cond = (alphas > 1e-4).reshape(-1)
-        # b = self.y[cond] - np.dot(self.x[cond], w)
-        b_vector = self.y - np.dot(self.x, w)
+        # b = self.train_target[cond] - np.dot(self.train_data[cond], w)
+        b_vector = self.train_target - np.dot(self.train_data, w)
         b = b_vector.sum() / b_vector.size
 
         # normalize
@@ -53,14 +53,14 @@ class DiverseLinearSVM(Classifier):
         # self.b = b
         self.b = b
 
-    def read_data(self, dataname):
+    def read_data(self, data_name):
         """
         Take values from a pickle.
 
-        :param dataname:
+        :param data_name:
         :return:
         """
-        self.x, self.y = read_data(dataname)
+        self.train_data, self.train_target = read_data(data_name)
 
     def generate_data(self,
                       m_1=1.0,
@@ -69,11 +69,17 @@ class DiverseLinearSVM(Classifier):
                       c_2=0.2,
                       num=50,
                       dim=2,
-                      dataname='gaussiandata.pickle'):
+                      data_name='gaussiandata.pickle'):
         """
-        Generate random binary data and save it into a pickle.
+        Generate random binartrain_target data and save it into a pickle.
 
-        :param dataname: the name of the pickle with the data.
+        :param m_1:
+        :param m_2:
+        :param c_1:
+        :param c_2:
+        :param num:
+        :param dim:
+        :param data_name:
         :return:
         """
         m_1 = m_1 * np.ones((dim,))
@@ -82,48 +88,48 @@ class DiverseLinearSVM(Classifier):
         c_2 = np.diag(c_2 * np.ones((dim,)))
 
         # generate points for class 1
-        x1 = generate_gaussian(m_1, c_1, num)
+        train_data1 = generate_gaussian(m_1, c_1, num)
         # generate points for class 2
-        x2 = generate_gaussian(m_2, c_2, num)
+        train_data2 = generate_gaussian(m_2, c_2, num)
         # labels
-        y1 = np.ones((x1.shape[0],))
-        y2 = -np.ones((x2.shape[0],))
+        train_target1 = np.ones((train_data1.shape[0],))
+        train_target2 = -np.ones((train_data2.shape[0],))
         # join
-        x = np.concatenate((x1, x2), axis=0)
-        y = np.concatenate((y1, y2), axis=0)
-        logging.debug('x {} y {}'.format(x.shape, y.shape))
-        # plot_data_with_labels(x, y)
+        train_data = np.concatenate((train_data1, train_data2), axis=0)
+        train_target = np.concatenate((train_target1, train_target2), axis=0)
+        logging.debug('train_data {} train_target {}'.format(train_data.shape, train_target.shape))
+        # plot_data_with_labels(train_data, train_target)
         # write
-        with open(dataname, 'wb') as f:
-            pickle.dump((x, y), f)
+        with open(data_name, 'wb') as f:
+            pickle.dump((train_data, train_target), f)
         # Instance
-        self.x = x
-        self.y = y
+        self.train_data = train_data
+        self.train_target = train_target
 
-    def add_x_y(self, x, y):
+    def add_train(self, train_data, train_target):
         """
-        Add the arrays directly.
+        Add the arrays directltrain_target.
 
-        :param x:
-        :param y:
+        :param train_data:
+        :param train_target:
         :return:
         """
-        self.x, self.y = x, y
+        self.train_data, self.train_target = train_data, train_target
 
-    def plot_data_separator(self, figname='dvsm.png'):
+    def plot_data_separator(self, fig_name='dvsm.png'):
         """
         Plot the data and the vector.
 
-        :param figname: Name in order to save the fig.
+        :param fig_name: Name in order to save the fig.
         """
         if not 'fig' in self.__dict__:
             self.fig, self.ax = plt.subplots()
-            # plot_data_with_labels(self.x, self.y, self.ax)
-            plot_data_with_labels(self.x, self.y)
+            # plot_data_with_labels(self.train_data, self.train_target, self.ax)
+            plot_data_with_labels(self.train_data, self.train_target)
         plot_separator(self.ax, self.w, self.b)
         if self.prev_w is not None:
             plot_separator(self.ax, self.prev_w, self.b)
-        plt.savefig(figname)
+        plt.savefig(fig_name)
         # plt.show()
         # plt.close()
 
