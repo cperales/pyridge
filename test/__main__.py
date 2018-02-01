974ab71c1369f127da417b6895117a7eda9a4c69
cperales <cperales@uloyola.es>
2018-02-01 20:51:00 +0100

    Run multiple tests in multiprocessing

--- test/__main__.py
+++ test/__main__.py
@@ -1,16 +1,29 @@
+# Tests
 from test_NELM import test_newthyroid as test_NELM
 from test_KELM import test_newthyroid as test_KELM
 from test_AdaBoostNELM import test_newthyroid as test_AdaBoostNELM
+
+# Other libraries
+from multiprocessing import Process
 import logging
 
 logger_pyelm = logging.getLogger('PyELM')
-logger_pyelm.setLevel(logging.DEBUG)
+logger_pyelm.setLevel(logging.INFO)
 
 logging.info('Starting tests...')
 logging.info('Running Kernel Extreme Learning Machine test')
-test_KELM()
+p_k = Process(target=test_KELM)
+p_k.start()
 logging.info('Running Neural Extreme Learning Machine test')
-test_NELM()
+p_n = Process(target=test_NELM)
+p_n.start()
 logging.info('Running AdaBoost Neural Extreme Learning Machine test')
-test_AdaBoostNELM()
+p_an = Process(target=test_AdaBoostNELM)
+p_an.start()
+
 logging.info('Finishing tests...')
+p_k.join()
+p_n.join()
+p_an.join()
+
+logging.info('Tests have finished!')
