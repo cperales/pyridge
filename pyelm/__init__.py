0052e25ff8879fa60fdb9a1772eb3bd485ee6dac
cperales <cperales@uloyola.es>
2018-02-01 20:51:23 +0100

    Temporary, numpy warnings are supressed

--- pyelm/__init__.py
+++ pyelm/__init__.py
@@ -3,3 +3,6 @@ logger_pyelm = logging.Logger('PyELM')
 logger_pyelm.setLevel(logging.DEBUG)
 logging.basicConfig(level=logging.DEBUG)
 logger_pyelm.debug('Logger instanced')
+
+import warnings
+warnings.simplefilter('ignore')
