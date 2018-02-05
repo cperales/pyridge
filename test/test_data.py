577207afdf75984e838c76e3300592e942589c9e
cperales <cperales@uloyola.es>
2018-02-05 14:35:49 +0100

    Test reduced, logging does not appear in test

--- test/test_data.py
+++ test/test_data.py
@@ -14,23 +14,3 @@ n_targ = train_j_target.shape[1]
 test_data, test_j_target = prepare_data(folder=folder,
                                         dataset=test_dataset,
                                         n_targ=n_targ)
-
-# Algorithm
-metric = metric_dict['accuracy']
-algorithm = algorithm_dict['KELM']
-C_range = [10**i for i in range(-2, 3)]
-k_range = [10**i for i in range(-2, 3)]
-kernel_fun = 'rbf'
-
-hyperparameters = {'kernelFun': kernel_fun,
-                   'C': C_range,
-                   'k': k_range}
-
-clf = algorithm()
-clf.set_cv_range(hyperparameters)
-cross_validation(classifier=clf, train_data=train_data, train_target=train_j_target)
-pred_targ = clf.predict(test_data=test_data)
-acc = metric(pred_targ=pred_targ,
-             real_targ=test_j_target)
-
-print('Finished!')
