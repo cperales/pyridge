5c79393fe7a520495edde860c7d9d324dc313334
cperales <cperales@uloyola.es>
2018-02-05 15:22:05 +0100

    Test as a script, not as a function

--- test/test_kelm.py
+++ test/test_kelm.py
@@ -3,50 +3,38 @@ from pyelm.algorithm import algorithm_dict
 from pyelm.utils import cross_validation
 from pyelm.utils.preprocess import prepare_data
 
-import logging
 
-logger = logging.getLogger('PyELM')
-logger.setLevel(logging.DEBUG)
+# Data
+folder = 'data/newthyroid'
+train_dataset = 'train_newthyroid.0'
+train_data, train_j_target = prepare_data(folder=folder,
+                                          dataset=train_dataset)
+test_dataset = 'test_newthyroid.0'
+n_targ = train_j_target.shape[1]
 
+test_data, test_j_target = prepare_data(folder=folder,
+                                        dataset=test_dataset,
+                                        n_targ=n_targ)
 
-def test_newthyroid():
-    """
-    Simple test with a UCI database.
-    """
-    # Data
-    folder = 'data/newthyroid'
-    train_dataset = 'train_newthyroid.0'
-    train_data, train_j_target = prepare_data(folder=folder,
-                                              dataset=train_dataset)
-    test_dataset = 'test_newthyroid.0'
-    n_targ = train_j_target.shape[1]
+# Algorithm 1
+metric = metric_dict['accuracy']
+algorithm = algorithm_dict['KELM']
+C_range = [10**i for i in range(-2, 3)]
+k_range = [10**i for i in range(-2, 3)]
+kernel_fun = 'rbf'
 
-    test_data, test_j_target = prepare_data(folder=folder,
-                                            dataset=test_dataset,
-                                            n_targ=n_targ)
+hyperparameters = {'kernelFun': kernel_fun,
+                   'C': C_range,
+                   'k': k_range}
 
-    # Algorithm
-    metric = metric_dict['accuracy']
-    algorithm = algorithm_dict['KELM']
-    C_range = [10**i for i in range(-2, 3)]
-    k_range = [10**i for i in range(-2, 3)]
-    kernel_fun = 'rbf'
-
-    hyperparameters = {'kernelFun': kernel_fun,
-                       'C': C_range,
-                       'k': k_range}
-
-    clf = algorithm()
-    clf.set_cv_range(hyperparameters)
-    cross_validation(classifier=clf, train_data=train_data, train_target=train_j_target)
-    pred_targ = clf.predict(test_data=test_data)
-    acc = metric(pred_targ=pred_targ,
-                 real_targ=test_j_target)
-
-    logger.info('Accuracy for algorithm KELM and dataset newthyroid.0,'
-                ' is {}'.format(acc))
-
-
-if __name__ == '__main__':
-    test_newthyroid()
-
+# Algorithm 2
+clf = algorithm()
+# Algorithm 3
+clf.set_cv_range(hyperparameters)
+# Algorithm 4
+cross_validation(classifier=clf, train_data=train_data, train_target=train_j_target)
+# Algorithm 5
+pred_targ = clf.predict(test_data=test_data)
+# Algorithm 6
+acc = metric(pred_targ=pred_targ,
+             real_targ=test_j_target)
